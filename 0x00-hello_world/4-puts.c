31654e7a9795837c6e3eaecba040064535824b9e
oluwatobi94 <oluwatobiadesanya@gmail.com>
2022-10-18 06:36:40 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,24 +1,33 @@
-nclude <stdio.h>
+uth: Brennan D Baraban <375@holbertonschool.com>
+
+ */
 
 
 
-/**
- *
- *  * main - Prints "Programming is like building a multilingual
- *
- *   *                puzzle, followed by a new line.
- *
- *    *
- *
- *     * Return: Always 0.
- *
- *      */
+#include <stdio.h>
 
-int main(void)
+
+
+ /**
+  *
+  *  * main - Prints "Programming is like building a multilingual
+  *
+  *   *                puzzle, followed by a new line.
+  *
+  *    *
+  *
+  *     * Return: Always 0.
+  *
+  *      */
+
+ int main(void)
 
 {
-	puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+
+
+
+			return (0);
 
 }
