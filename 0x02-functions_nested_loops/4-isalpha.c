590e95e796adf4b5f6196f521602a03c2921b242
oluwatobi94 <oluwatobiadesanya@gmail.com>
2022-10-19 17:38:03 -0700

    task 5

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,18 +1,21 @@
 #include "main.h"
 /**
- *  * _isalpha - main function
- *   *
- *    * @c: character to compare
- *     *
- *      * Return: 1 if true, 0 if false
- *       *
- *        */
+ * _isalpha - main function
+ *   
+ * @c: character to compare
+ * 
+ * Return: 1 if true, 0 if false
+ *
+ */
 
 int _isalpha(int c)
 {
-		if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-				{
-							return (1);
-								}
-			return (0);
+         if ((c >= 'a' && c <= 'z')
+ || (c >= 'A' && c <= 'Z'))
+			
+        {
+         
+		return (1);
+        }
+        return (0);
 }
